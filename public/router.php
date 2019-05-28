33aa8e50b75ca30bee3abc7d4241f89799ec8cd7
Simon <6692223@qq.com>
2019-05-28 16:24:27 +0800

    4.2

--- public/router.php
+++ public/router.php
@@ -9,6 +9,9 @@
 // | Author: liu21st <liu21st@gmail.com>
 // +----------------------------------------------------------------------
 // $Id$
-use think\Route;
 
-Route::rule('hello' , 'sample/Test/hello');
+if (is_file($_SERVER["DOCUMENT_ROOT"] . $_SERVER["REQUEST_URI"])) {
+    return false;
+} else {
+    require __DIR__ . "/index.php";
+}
