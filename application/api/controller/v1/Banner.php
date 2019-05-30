a26c7d27f4be637d17a29bcea85266be34def1a1
Simon <6692223@qq.com>
2019-05-30 18:08:18 +0800

    first ORM

--- application/api/controller/v1/Banner.php
+++ application/api/controller/v1/Banner.php
@@ -17,7 +17,7 @@ class Banner
     public function getBanner($id)
     {
         (new IDMustBePostiveInt())->goCheck();
-        $banner = BannerModel::getBannerByID($id);
+        $banner = BannerModel::get($id);
         if(!$banner){
             throw new Exception('内部错误');
         }
