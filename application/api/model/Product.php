2fa45652adbc817f1f79a89a50dcb70d7bdc6b19
Simon <6692223@qq.com>
2019-06-01 22:17:10 +0800

    write theme info

--- application/api/model/Product.php
+++ application/api/model/Product.php
@@ -4,5 +4,5 @@ namespace app\api\model;
 
 class Product extends BaseModel
 {
-    //
+    
 }
--- application/api/model/Theme.php
+++ application/api/model/Theme.php
@@ -21,4 +21,11 @@ class Theme extends BaseModel
     {
         return $this->belongsToMany('Product' , 'theme_product' , 'product_id' , 'theme_id');
     }
+
+    public static function getThemeWithProducts($id)
+    {
+        $theme = self::with('products,topicImg,headImg')
+            ->find($id);
+        return $theme;
+    }
 }
