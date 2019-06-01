44c2446ff658bba4080adacf55e70d07e2b36f74
Simon <6692223@qq.com>
2019-06-01 22:34:27 +0800

    database

--- application/api/model/Product.php
+++ application/api/model/Product.php
@@ -4,5 +4,9 @@ namespace app\api\model;
 
 class Product extends BaseModel
 {
-    
+    protected $hidden = [
+        'delete_time' , 'main_img_id' , 'pivot'
+        ,'from' , 'category_id' , 'create_time'
+        ,'update_time'
+    ];
 }
