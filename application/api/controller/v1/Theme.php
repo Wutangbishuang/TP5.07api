650390f9f16e08dc09a3b69d9315a04918b1d65a
Simon <6692223@qq.com>
2019-06-01 17:34:58 +0800

    rebuild theme api

--- application/api/controller/v1/Theme.php
+++ application/api/controller/v1/Theme.php
@@ -2,10 +2,20 @@
 
 namespace app\api\controller\v1;
 
+use app\api\validate\IDCollection;
 use think\Controller;
 use think\Request;
 
 class Theme extends Controller
 {
+    /**
+     * @url /theme?ids=id1 , id2 , id3 , ...
+     * @return 一组 Theme 模型
+     */
 
+    public function getSimpleList($ids = '')
+    {
+        (new IDCollection())->goCheck();
+        return 'success';
+    }
 }
--- application/route.php
+++ application/route.php
@@ -2,3 +2,4 @@
 use think\Route;
 
 Route::get('api/:version/banner/:id' , 'api/:version.Banner/getBanner');
+Route::get('api/:version/theme' , 'api/:version.Theme/getSimpleList');
