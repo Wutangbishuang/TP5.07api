e4636ef5766aaadfa9330aba2d81b1672678535c
Simon <6692223@qq.com>
2019-06-02 10:29:54 +0800

    program category controller , model and exception

--- application/route.php
+++ application/route.php
@@ -2,6 +2,9 @@
 use think\Route;
 
 Route::get('api/:version/banner/:id' , 'api/:version.Banner/getBanner');
+
 Route::get('api/:version/theme' , 'api/:version.Theme/getSimpleList');
 Route::get('api/:version/theme/:id' , 'api/:version.Theme/getComplexOne');
-Route::get('api/:version/product' , 'api/:version.product/getRecent');
+
+Route::get('api/:version/product/recent' , 'api/:version.product/getRecent');
+Route::get('api/:version/category/all' , 'api/:version.Category/getAllCategories');
