f4e0ed7ea488992256b697880fae09831eecf895
Simon <6692223@qq.com>
2019-06-03 23:28:51 +0800

    product info api

--- application/api/controller/v1/Product.php
+++ application/api/controller/v1/Product.php
@@ -34,4 +34,14 @@ class Product
         return $products;
     }
 
+    public function getOne($id)
+    {
+        (new IDMustBePostiveInt())->goCheck();
+        $product = ProductModel::getProductDetail($id);
+        if(!$product){
+            throw new ProductException();
+        }
+        return $product;
+    }
+
 }
--- application/api/model/Product.php
+++ application/api/model/Product.php
@@ -15,6 +15,16 @@ class Product extends BaseModel
         return $this->prefixImgUrl($value , $data);
     }
 
+    public function imgs()
+    {
+         return $this->hasMany('ProductImage' , 'product_id' , 'id');
+    }
+
+    public function properties()
+    {
+        return $this->hasMany('ProductProperty' , 'product_id' , 'id');
+    }
+
     public function getMostRecent($count)
     {
         $products = self::limit($count)
@@ -28,5 +38,12 @@ class Product extends BaseModel
         $products = self::where('category_id' , '=' , $categoryID)
             ->select();
         return $products;
+    }
+
+    public static function getProductDetail($id)
+    {
+        $product = self::with('imgs,properties')
+        ->find($id);
+        return $product;
     }
 }
