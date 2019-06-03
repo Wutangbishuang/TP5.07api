560af2ef7ce91895c507409ddf994482ae56e2dd
Simon <6692223@qq.com>
2019-06-03 09:55:18 +0800

    program wx.login api step 2

--- application/api/service/UserToken.php
+++ application/api/service/UserToken.php
@@ -4,10 +4,38 @@
 namespace app\api\service;
 
 
+use think\Exception;
+
 class UserToken
 {
+    protected $code;
+    protected $wxAppId;
+    protected $wxAppSecret;
+    protected $wxLoginUrl;
+
+    function __construct($code)
+    {
+        $this->$code = $code;
+        $this->wxAppID = config('wx.app_id');
+        $this->wxAppSecret = config('wx.app_secret');
+        $this->wxLoginUrl = sprintf(config('wx.login_url'),
+            $this->wxAppId,$this->wxAppSecret,$this->code);
+    }
+
+
     public function get($code)
     {
+        $result = curl_get($this->wxLoginUrl);
+        $wxResult = json_decode($result , true);
+        if(empty($wxResult)){
+            throw new Exception('获取session_key及openID时异常 , 微信内部错误');
+        } else {
+                $loginFail = array_key_exists('errcode', $wxResult);
+                if ($loginFail) {
 
+                } else {
+    
+                }
+        }
     }
 }
---
+++ application/extra/wx.php
@@ -0,0 +1,8 @@
+<?php
+
+
+return [
+    'appid' => 'wx1dd75bb5ba4b9cb5',
+    'app_secret' => '191bd1e7dfc781ccd87082af9577bd67',
+    'login_url' => 'GET https://api.weixin.qq.com/sns/jscode2session?appid=%s&secret=%s&js_code=%s&grant_type=authorization_code'
+];
