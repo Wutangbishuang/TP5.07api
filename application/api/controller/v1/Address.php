66569b02b50e6c65f74da0669c55049156996ead
Simon <6692223@qq.com>
2019-06-04 15:33:29 +0800

    create or update user address

--- application/api/controller/v1/Address.php
+++ application/api/controller/v1/Address.php
@@ -4,8 +4,11 @@
 namespace app\api\controller\v1;
 
 
+use app\api\model\User as UserModel;
 use app\api\validate\AddressNew;
 use app\api\service\Token as TokenService;
+use app\lib\exception\SuccessMessage;
+use app\lib\exception\UserException;
 
 class Address
 {
@@ -16,6 +19,22 @@ class Address
         // 根据uid来查找用户数据 , 判断用户是否存在 , 如果不存在抛出异常
         // 获取用户从客户端提交来的地址信息
         // 根据用户地址信息是否存在 , 从而判断是 添加地址 还是 更新地址
+
         $uid = TokenService::getCurrentUid();
+        $user = UserModel::get($uid);
+        if(!$user){
+            throw new UserException();
+        }
+        $dataArray = getDatas();
+        $userAddress = $user->address();
+        if(!$userAddress){
+            // 创建 注意address()
+            $user->address()->save($dataArray);
+        } else {
+            // 更新 注意address
+            $user->address->save($dataArray);
+        }
+        return new SuccessMessage();
+
     }
 }
