4f56bbf3df5d9496bd93e2604abb2073d6521395
Simon <6692223@qq.com>
2019-06-05 17:50:52 +0800

    before action

--- application/api/controller/v1/Address.php
+++ application/api/controller/v1/Address.php
@@ -9,9 +9,13 @@ use app\api\validate\AddressNew;
 use app\api\service\Token as TokenService;
 use app\lib\exception\SuccessMessage;
 use app\lib\exception\UserException;
+use think\Controller;
 
-class Address
+class Address extends Controller
 {
+    protected $beforeActionList = [
+
+    ];
     public function createOrUpdateAddress()
     {
         $validate = new AddressNew();
