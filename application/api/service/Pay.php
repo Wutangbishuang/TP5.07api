3c362d55bebf98e9777890628d95dd47d82e5f90
Simon <6692223@qq.com>
2019-06-21 00:16:17 +0800

    wechat pay step 3

--- application/api/service/Pay.php
+++ application/api/service/Pay.php
@@ -4,7 +4,12 @@
 namespace app\api\service;
 
 
+use app\lib\enum\OrderStatusEnum;
+use app\lib\exception\OrderException;
+use app\lib\exception\TokenException;
 use think\Exception;
+use app\api\model\Order as OrderModel;
+use app\api\service\Order as OrderService;
 
 class Pay
 {
@@ -21,7 +26,46 @@ class Pay
 
     public function pay()
     {
-        
+        // 订单号可能根本就不存在
+        // 订单号确实是存在的 , 但是 , 订单号和当前用户是不匹配的
+        // 订单有可能已经被支付过
+        // 进行库存量检测
+        $this->checkOrderValid();
+        $orderService = new OrderService();
+        $status = $orderService->checkOrderStock($this->orderID);
+        if(!$status['pass']){
+            return $status;
+        }
+    }
+
+    private function makeWxPreOrder()
+    {
+
+    }
+
+    private function checkOrderValid()
+    {
+        $order = OrderModel::where('id' , '=' , $this->orderId)
+            ->find();
+        if(!$order){
+            throw new OrderException();
+        }
+
+        if(!Token::isValidOperate($order->user_id)){
+            throw new TokenException([
+                'msg' => '订单与用户不匹配',
+                'errorCode' => 10003
+            ]);
+        }
+        if($order->status != OrderStatusEnum::UNPAID){
+            throw new OrderException([
+                'msg' => '订单已支付过啦',
+                'errorCode' => 80003,
+                'code' => 400
+            ]);
+        }
+        $this->orderNO = $order->order_no;
+        return true;
     }
 
 
