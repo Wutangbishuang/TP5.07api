9eef5a47564727bd873333dc1ae22385eb635753
Simon <6692223@qq.com>
2019-06-23 00:31:07 +0800

    order API Done

--- application/api/controller/v1/Order.php
+++ application/api/controller/v1/Order.php
@@ -6,9 +6,11 @@ namespace app\api\controller\v1;
 
 use app\api\service\Order as OrderService;
 use app\api\service\Token as TokenService;
+use app\api\validate\IDMustBePostiveInt;
 use app\api\validate\OrderPlace;
 use app\api\validate\PagingParameter;
 use app\api\model\Order as OrderModel;
+use app\lib\exception\OrderException;
 
 class Order extends BaseController
 {
@@ -25,7 +27,7 @@ class Order extends BaseController
 
     protected $beforeActionList = [
         'checkExclusiveScope' => ['only' =>'placeOrder'],
-        'checkPrimaryScope' => ['only' => 'getSummaryByUser']
+        'checkPrimaryScope' => ['only' => 'getDetail,getSummaryByUser']
     ];
 
     public function getSummaryByUser($page=1 , $size=15)
@@ -44,6 +46,18 @@ class Order extends BaseController
             'data' => $data,
             'current_page' => $pagingOrders->getCurrentPage()
         ];
+    }
+
+    public function getDetail($id)
+    {
+        (new IDMustBePostiveInt())->goCheck();
+        $orderDetail = OrderModel::get($id);
+        if(!$orderDetail)
+        {
+            throw new OrderException();
+        }
+        return $orderDetail
+            ->hidden(['prepay_id']);
     }
     
     public function placeOrder()
